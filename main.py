2b46bc07366123031b8d1c6e69106bb9abf0c132
Hammad335 <hamadmemon946@gmail.com>
2026-09-20 16:04:50 +0500

    - initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from app.routes.issues import router as issues_router
+
+app = FastAPI()
+
+app.include_router(issues_router)
+
+
+
+
+
+# items = [
+#     {'id': 1, 'name': 'item 1'},
+#     {'id': 2, 'name': 'item 2'},
+#     {'id': 3, 'name': 'item 3'},
+# ]
+
+
+# @app.get("/health")
+# def health_check():
+#     return {"status": "ok"}
+
+# @app.get('/items')
+# def get_items():
+#     return items
+
+# @app.get('/items/{item_id}')
+# def get_item(item_id: int):
+#     for item in items:
+#         if item['id'] == item_id:
+#             return item
+#     return {'error': 'Item not found'}
+
+# @app.post('/item')
+# def create_item(item: dict):
+#     items.append(item)
+#     return item
